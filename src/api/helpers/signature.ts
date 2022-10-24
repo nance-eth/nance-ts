79b92f08b65819d591d359f9c26b5a69e7dbc7ac
jigglyjams <jigglyjams@protonmail.com>
2022-10-23 21:26:49 -0700

    bug: whats the URL path

--- src/api/helpers/signature.ts
+++ src/api/helpers/signature.ts
@@ -3,11 +3,11 @@ import { DOMAIN, TYPES } from '../constants/Signature';
 import { Signature } from '../../types';
 
 const getApiPath = () => {
-  console.log(process.env.RAILWAY_STATIC_URL);
-  return `https://${process.env.RAILWAY_STATIC_URL}` || 'http://localhost:3000';
+  return `https://${process.env.RAILWAY_CUSTOM_URL}` || 'http://localhost:3000';
 };
 
 export function checkSignature(signaturePacket: Signature, space: string, command: string, payload: any) {
+  console.log(getApiPath());
   const typedValue = {
     path: `${getApiPath()}/${space}/${command}`,
     timestamp: signaturePacket.timestamp,
