187a093a1c459624993f36766a0d5affd26296e8
jigglyjams <jigglyjams@protonmail.com>
2022-11-08 08:14:29 -0800

    api examples: update to sign upload request

--- src/api/examples/basic.ts
+++ src/api/examples/basic.ts
@@ -5,7 +5,6 @@ import {
   ProposalMarkdownResponse,
   APIErrorResponse
 } from '../models';
-import { Proposal } from '../../types';
 
 const API_STAGING = 'https://nance-ts-staging.up.railway.app';
 const API_MAIN = 'https://api.nance.app';
@@ -14,35 +13,6 @@ const API = API_STAGING;
 
 const SPACE = 'waterbox';
 const PROPOSAL_HASH = 'd2ef2ae69d2a4cff945c2677b73186b9';
-
-const exampleProposal: Proposal = {
-  hash: '',
-  status: '',
-  type: 'Payout',
-  governanceCycle: 33,
-  proposalId: '',
-  author: '',
-  version: 'V1',
-  title: 'New Proposal by jigglyjams',
-  // eslint-disable-next-line max-len
-  body: 'Status: Draft\n\n```\nAuthor:\nDate: (YYYY-MM-DD)\n```\n\n## Synopsis\n\n*State what the proposal does in one sentence.*\n\n## Motivation\n\n*What problem does this solve? Why now?* \n\n## Specification\n\n*How exactly will this be executed? Be specific and leave no ambiguity.* \n\n## Rationale\n\n*Why is this specification appropriate?*\n\n## Risks\n\n*What might go wrong?*\n\n## Timeline\n\n*When exactly should this proposal take effect? When exactly should this proposal end?*',
-  discussionThreadURL: '',
-  ipfsURL: '',
-  voteURL: '',
-  url: '',
-  notification: {
-    discordUserId: '',
-    expiry: false,
-    execution: false,
-    progress: false
-  },
-  payout: {
-    type: 'address',
-    count: 3,
-    amountUSD: 1800,
-    address: '0x25910143C255828F623786f46fe9A8941B7983bB',
-  }
-};
 
 const getSpaceInfo = async (space: string): Promise<SpaceInfoResponse | APIErrorResponse> => {
   return axios.get(`${API}/${space}`).then((response) => {
@@ -62,14 +32,6 @@ const queryMarkdown = async (space: string, hash: string): Promise<ProposalMarkd
   });
 };
 
-async function uploadProposal(space: string, proposal: Proposal): Promise<ProposalMarkdownResponse | APIErrorResponse> {
-  return axios.post(`${API}/${space}/upload`, {
-    proposal
-  }).then((response) => {
-    return response.data;
-  });
-}
-
 async function example() {
   const spaceInfoResponse = await getSpaceInfo(SPACE);
   const proposalsResponse = await queryProposals(SPACE);
@@ -78,10 +40,6 @@ async function example() {
   console.log(spaceInfoResponse);
   console.log(proposalsResponse);
   console.log(specificProposalResponse);
-
-  // upload proposal
-  const uploadProposalResponse = await uploadProposal(SPACE, exampleProposal);
-  console.log(uploadProposalResponse);
 }
 
 example();
---
+++ src/api/examples/upload.ts
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { ethers } from 'ethers';
+import { Proposal, Signature } from '../../types';
+import { DOMAIN, TYPES } from '../constants/Signature';
+import {
+  ProposalMarkdownResponse,
+  APIErrorResponse
+} from '../models';
+
+const API_STAGING = 'https://nance-ts-staging.up.railway.app';
+const API_MAIN = 'https://api.nance.app';
+const API_LOCAL = 'http://localhost:3000';
+const API = API_LOCAL;
+
+const SPACE = 'waterbox';
+const PROPOSAL: Proposal = {
+  hash: '',
+  status: 'Draft',
+  type: 'Payout',
+  governanceCycle: undefined,
+  proposalId: '',
+  author: '',
+  version: '1',
+  title: 'Wiggly wiggly woo',
+  // eslint-disable-next-line max-len
+  body: 'Status: Draft\n\n```\nAuthor:\nDate: (YYYY-MM-DD)\n```\n\n## Synopsis\n\n*State what the proposal does in one sentence.*\n\n## Motivation\n\n*What problem does this solve? Why now?* \n\n## Specification\n\n*How exactly will this be executed? Be specific and leave no ambiguity.* \n\n## Rationale\n\n*Why is this specification appropriate?*\n\n## Risks\n\n*What might go wrong?*\n\n## Timeline\n\n*When exactly should this proposal take effect? When exactly should this proposal end?*',
+  discussionThreadURL: '',
+  ipfsURL: '',
+  voteURL: '',
+  url: '',
+  payout: {
+    type: 'address',
+    count: 3,
+    amountUSD: 1800,
+    address: '0x25910143C255828F623786f46fe9A8941B7983bB',
+  }
+};
+
+async function signPayload(space: string, command: string, payload: any): Promise<Signature> {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const path = `${API}/${space}/${command}`;
+  const typedValue = {
+    path,
+    timestamp,
+    payload: ethers.utils.solidityKeccak256(['string'], [JSON.stringify(payload)])
+  };
+  const wallet = ethers.Wallet.createRandom();
+  // eslint-disable-next-line no-underscore-dangle
+  return wallet._signTypedData(DOMAIN, TYPES, typedValue).then((signature) => {
+    return {
+      address: wallet.address,
+      signature,
+      timestamp
+    };
+  });
+}
+
+async function uploadProposal(space: string, proposal: Proposal): Promise<ProposalMarkdownResponse | APIErrorResponse> {
+  const signature = await signPayload(space, 'upload', proposal);
+  return axios.post(`${API}/${space}/upload`, {
+    signature,
+    proposal
+  }).then((response) => {
+    return response.data;
+  });
+}
+
+// upload
+console.log('Upload proposal...');
+uploadProposal(SPACE, PROPOSAL).then((res) => {
+  console.log(res);
+});
