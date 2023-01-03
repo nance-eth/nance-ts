51ebb0d847cb3d00cad30110c7998412e3db6592
jigglyjams <jigglyjams@protonmail.com>
2023-01-03 11:26:52 -0800

    feature: api: merge current event into main

--- src/calendar/tests/calendar.ts
+++ src/calendar/tests/calendar.ts
@@ -1,7 +1,8 @@
 import { CalendarHandler } from '../CalendarHandler';
 
-const calendar = new CalendarHandler('./src/config/juicebox/juicebox.ics');
+const calendar = new CalendarHandler('./src/config/jigglyjams/jigglyjams.ics');
 // calendar.useIcsLinkInstead('https://raw.githubusercontent.com/jigglyjams/nance/main/tmp/juicebox.ics').then(() => {
 //   console.log(calendar.getNextEvent());
 // });
-console.log(calendar.getNextEvents());
+// console.log(calendar.getNextEvents());
+console.log(calendar.getCurrentEvent());
