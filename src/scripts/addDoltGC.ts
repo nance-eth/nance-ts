fa5d5a8852aa23167eb5a1355c596c38a971377b
jigglyjams <jigglyjams@protonmail.com>
2023-01-26 22:57:03 -0800

    cleanup: update script to use new nance functionality to increase GC

--- src/scripts/addDoltGC.ts
+++ src/scripts/addDoltGC.ts
@@ -2,32 +2,17 @@ import { NanceTreasury } from '../treasury';
 import { getConfig } from '../configLoader';
 import { NotionHandler } from '../notion/notionHandler';
 import { DoltHandler } from '../dolt/doltHandler';
-import { addSecondsToDate } from '../utils';
-import { GovernanceCycle } from '../dolt/schema';
+import { Nance } from '../nance';
 
 async function main(){
   const config = await getConfig();
-  const notion = new NotionHandler(config);
+  const nance = new Nance(config);
   const dolt = new DoltHandler(
     { database: config.dolt.repo, host: process.env.DOLT_HOST, port: Number(process.env.DOLT_PORT), user: process.env.DOLT_USER, password: process.env.DOLT_PASSWORD }, config.propertyKeys
   );
   const treasury = new NanceTreasury(config, dolt);
-  const { number: numberV2} = await treasury.getQueuedConfiguration('V2');
-  const { number: numberV3, start: startV3, duration: durationV3 } = await treasury.getQueuedConfiguration('V3');
-  const startDatetime = addSecondsToDate(new Date(startV3.toNumber() * 1000), durationV3.toNumber());
-  const cycleNumber = await notion.getCurrentGovernanceCycle() + 1;
-  console.log(await dolt.getCurrentGovernanceCycle());
-  const governance: GovernanceCycle = {
-    cycleNumber,
-    startDatetime,
-    endDatetime: addSecondsToDate(startDatetime, durationV3.toNumber()),
-    jbV1FundingCycle: cycleNumber,
-    jbV2FundingCycle: numberV2.toNumber() + 1,
-    jbV3FundingCycle: numberV3.toNumber() + 1,
-    acceptingProposals: true
-  }
-  console.log(governance);
-  // dolt.addGovernanceCycleToDb(governance);
+  const governanceCycle = await treasury.getCycleInformation();
+  nance.incrementGovernanceCycle(governanceCycle);
 }
 
 main();
