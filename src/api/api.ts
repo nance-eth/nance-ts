64f33878f5d345af62393e6d0dd10d4b75cb4e29
jigglyjams <jigglyjams@protonmail.com>
2023-03-07 09:55:13 -0700

    bug: return error if no proposal found

--- src/api/api.ts
+++ src/api/api.ts
@@ -15,6 +15,7 @@ import { DiscordHandler } from '../discord/discordHandler';
 import { keys } from '../keys';
 import { dbOptions } from '../dolt/dbConfig';
 import { SQLPayout } from '../dolt/schema';
+import { Proposal } from '../types';
 
 const router = express.Router();
 const spacePrefix = '/:space';
@@ -122,7 +123,8 @@ router.get(`${spacePrefix}/proposal/:pid`, async (req, res) => {
   const { pid } = req.params;
   const { proposalHandlerBeta } = res.locals;
   return res.send(
-    await proposalHandlerBeta.getProposalByAnyId(pid).then((proposal: string) => {
+    await proposalHandlerBeta.getProposalByAnyId(pid).then((proposal: Proposal[]) => {
+      if (proposal.length === 0) return { success: false, error: 'proposal not found' };
       return { sucess: true, data: proposal[0] };
     }).catch((e: any) => {
       return { success: false, error: e };
