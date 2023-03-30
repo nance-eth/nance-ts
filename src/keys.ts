6808571c63b4b27d5ffc516ea785b48e254a0684
jigglyjams <jigglyjams@protonmail.com>
2023-03-30 14:26:06 -0400

    logistics: migrate off of nft.storage, to infura ipfs

--- src/keys.ts
+++ src/keys.ts
@@ -3,6 +3,8 @@ import 'dotenv/config';
 export const keys = {
   PROVIDER_KEY: process.env.PROVIDER_KEY ?? '',
   INFURA_KEY: process.env.INFURA_KEY ?? '',
+  INFURA_IPFS_ID: process.env.INFURA_IPFS_ID ?? '',
+  INFURA_IPFS_SECRET: process.env.INFURA_IPFS_SECRET ?? '',
   DEEPL_KEY: process.env.DEEPL_KEY ?? '',
   PRIVATE_KEY: process.env.PRIVATE_KEY ?? '',
   GITHUB_KEY: process.env.GITHUB_KEY ?? '',
--- src/storage/storageHandler.ts
+++ src/storage/storageHandler.ts
@@ -1,23 +1,31 @@
 /* eslint-disable quote-props */
 import axios from 'axios';
+import FormData from 'form-data';
 import { keys } from '../keys';
 
-const API = 'https://api.nft.storage';
+const API = 'https://ipfs.infura.io:5001/api/v0';
 
+const AUTH_HEADER = `Basic ${Buffer.from(
+  `${keys.INFURA_IPFS_ID}:${keys.INFURA_IPFS_SECRET}`,
+).toString('base64')}`;
+
+console.log(AUTH_HEADER);
+
+// https://github.com/jbx-protocol/juice-interface/blob/main/src/lib/infura/ipfs.ts
 export async function dotPin(dataIn: string, encoding = 'utf-8' as BufferEncoding): Promise<string> {
   const data = Buffer.from(dataIn, encoding);
+  const formData = new FormData();
+  formData.append('file', data);
   return axios({
     method: 'post',
-    url: `${API}/upload`,
+    url: `${API}/add`,
     headers: {
-      'Authorization': `Bearer ${keys.STORAGE_KEY}`,
-      'Content-Type': '*/*',
-      'Accept-Encoding': '*',
-      'Content-Length': data.length
+      'Authorization': AUTH_HEADER,
+      'Content-Type': 'multipart/form-data',
     },
-    data
+    data: formData
   }).then((res) => {
-    return res.data.value.cid;
+    return res.data.Hash;
   }).catch((e) => {
     return Promise.reject(e);
   });
