4671c0707da4821baef99504ef24c370998c6248
jigglyjams <jigglyjams@protonmail.com>
2023-05-17 10:32:32 -0500

    cleanup: api examples

--- src/api/examples/edit.ts
+++ src/api/examples/edit.ts
@@ -1,38 +1,23 @@
 import axios from 'axios';
 import { signPayload, nanceWallet } from './helpers/signer';
-import { Proposal } from '../../types';
+import { Action, Proposal } from '../../types';
 
 const API_STAGING = 'https://api.staging.nance.app';
 const API_MAIN = 'https://api.nance.app';
 const API_LOCAL = 'http://localhost:3000';
 const API = API_LOCAL;
 
-const PROPOSAL: Proposal = {
-  hash: '5f8d8b4fffc843eea24470493fd989df',
-  status: 'Draft',
-  type: 'Payout',
-  governanceCycle: 40,
-  proposalId: null,
-  author: '',
-  title: 'DO the thing to the woot m',
-  // eslint-disable-next-line max-len
-  body: 'Status: Draft\n\n```\nAuthor:\nDate: (YYYY-MM-DD)\n```\n\n## Synopsis\n\n*State what the proposal does in one sentence.*\n\n## Motivation\n\n*What problem does this solve? Why now?* \n\n## Specification\n\n*How exactly will this be executed? Be specific and leave no ambiguity.* \n\n## Rationale\n\n*Why is this specification appropriate?*\n\n## Risks\n\n*What might go wrong?*\n\n## Timeline\n\n*When exactly should this proposal take effect? When exactly should this proposal end?*',
-  discussionThreadURL: 'd',
-  ipfsURL: '',
-  voteURL: '',
-  url: '',
-  payout: {
-    type: 'address',
-    count: 2,
-    amountUSD: 2800,
-    address: '0x25910143C255828F623786f46fe9A8941B7983bB',
-    payName: 'testing123 payout editted'
-  }
-};
+const SPACE = 'waterbox';
+const uuid = '0dd665bac6794fa98e6d064fb3351e07';
 
-async function main(space: string, proposal: Proposal) {
+async function main(space: string) {
+  const proposal = (await axios.get(`${API}/${space}/proposal/${uuid}`)).data.data;
+  console.log(proposal);
+  const reserveActionIndex = proposal.actions.findIndex((action: Action) => { return action.type === 'Reserve'; });
+  proposal.actions[reserveActionIndex].payload.splits[10].percent = 1;
+  console.log(proposal);
   const signature = await signPayload(space, 'edit', proposal);
   console.log((await axios.put(`${API}/${space}/proposal/${proposal.hash}`, { signature, proposal })).data);
 }
 
-main('waterbox', PROPOSAL);
+main('waterbox');
--- src/api/examples/upload.ts
+++ src/api/examples/upload.ts
@@ -57,7 +57,8 @@ const PROPOSAL: Proposal = {
         contract: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         tokenName: 'USDC',
         to: '0x25910143C255828F623786f46fe9A8941B7983bB',
-        amount: '1000000'
+        amount: '1000000',
+        decimals: 6
       }
     },
     {
