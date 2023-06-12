5da3f21a47dbec6c4d1131d010777fd1ef8e0557
twodam <twodam@protonmail.com>
2023-06-12 14:51:45 +0800

    fix: .

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,7 +9,8 @@ app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors({
   maxAge: 86400,
-  origin: 'http://localhost:3001'
+  origin: 'http://localhost:3001',
+  credentials: true
 }));
 app.set('json spaces', 2);
 
