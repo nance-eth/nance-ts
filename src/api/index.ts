2d7875b1bd2153b69227f4ee00bf279318447130
twodam <twodam@protonmail.com>
2023-06-12 15:42:34 +0800

    fix: .

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,7 +9,11 @@ app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors({
   maxAge: 86400,
-  origin: 'http://localhost:3001',
+  origin: [
+    'http://localhost:3001',
+    'https://jbdao.org',
+    'https://jbdao-org-git-nextauth-juicetool.vercel.app'
+  ],
   credentials: true
 }));
 app.set('json spaces', 2);
