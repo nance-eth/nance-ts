bde0089db1b1fa764d3cf5848f6a4ca310e8e9b1
jigglyjams <jigglyjams@protonmail.com>
2023-08-23 20:03:52 -0400

    bug: download images for discord image sender

--- src/api/helpers/auto/daily.ts
+++ src/api/helpers/auto/daily.ts
@@ -3,16 +3,18 @@ import { discordLogin } from '../discord';
 import { shouldIncrementDay } from './logic';
 import { DoltSysHandler } from '../../../dolt/doltSysHandler';
 import { pools } from '../../../dolt/pools';
+import { downloadImages } from '../../../utils';
 // import { juiceboxTime } from '../juicebox';
 
 const doltSys = new DoltSysHandler(pools.nance_sys);
 
-export const handleDailyCheck = async (space: SpaceAuto) => {
+export const handleDaily = async (space: SpaceAuto) => {
   if (shouldIncrementDay(space)) {
     await doltSys.incrementCycleDay(space.name);
     // reread space config to see if cycleCurrentDay needs to roll over and if governanceCycle was updated
     const recheck = await doltSys.getSpaceConfig(space.name);
     if (recheck) {
+      await downloadImages(space.name, space.config.discord.reminder.imagesCID, space.config.discord.reminder.imageNames);
       const { cycleCurrentDay, currentGovernanceCycle } = recheck;
       const dialogHandler = await discordLogin(space.config);
       const currentEvent = space.currentEvent.title.toLowerCase();
