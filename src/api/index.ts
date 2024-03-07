91b0585514b5d85ce71f6d0386b0c0af9ab60541
jigglyjams <jigglyjams@protonmail.com>
2024-03-07 18:32:06 -0500

    feat(rateLimit): improved

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ import { params } from './tspec';
 import api from './api';
 import ish from './nanceish';
 import tasks from './tasks';
-import { limiter, ipBan } from "./limiter";
+import { limiter } from "./limiter";
 
 const app = express();
 app.use(express.json({ limit: '20mb' }));
@@ -15,7 +15,6 @@ app.use(cors({
 }));
 
 app.set('trust proxy', 1);
-app.use(ipBan);
 app.use(limiter);
 
 app.set('json spaces', 2);
--- src/api/limiter.ts
+++ src/api/limiter.ts
@@ -3,20 +3,6 @@ import { Request, Response, NextFunction } from "express";
 
 const bannedIps: string[] = [];
 
-export const ipBan = (req: Request, res: Response, next: NextFunction) => {
-  const ip = req.ip || req.ips[0];
-  if (bannedIps.includes(ip)) {
-    console.error(`Banned IP address: ${ip}`);
-    console.error('Banned IPs:', bannedIps);
-    res.status(403).send('you banned.');
-    return;
-  }
-  console.log(`Client connected with IP address: ${req.ip || req.ips[0]}`);
-  console.log(`Request URL: ${req.url}`);
-  console.log('==============================================');
-  next();
-};
-
 export const limiter = rateLimit({
   // 20 seconds
   windowMs: 20 * 1000,
@@ -26,8 +12,13 @@ export const limiter = rateLimit({
   legacyHeaders: false,
   handler: (req, res) => {
     const ip = req.ip || req.ips[0];
+    if (bannedIps.includes(ip)) {
+      res.status(403).send('you banned.');
+      return;
+    }
     console.error(`Rate limit exceeded. IP: ${ip}, URL: ${req.url}`);
     bannedIps.push(ip);
+    console.error(`Banned IPs: ${bannedIps}`);
     res.status(429).send('too many requests.');
   },
 });
