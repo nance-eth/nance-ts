e924bfd6e8cb318a5cd7f0d8e068609313756701
jigglyjams <jigglyjams@protonmail.com>
2024-03-07 17:52:33 -0500

    feat(rateLimiter): trying

--- src/api/limiter.ts
+++ src/api/limiter.ts
@@ -1,14 +1,10 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-  // 100 requests per minute
-  windowMs: 1 * 60 * 1000,
-  max: 100,
+  // 20 seconds
+  windowMs: 20 * 1000,
+  max: 10,
   message: 'Too many requests from this IP, please try again later.',
-  keyGenerator: (req) => {
-    console.log('[RLRL] Request headers:', req.headers);
-    return req.headers['x-forwarded-for'] as string;
-  },
 });
 
 export const logHeadersOnRateLimit = (req: any, res: any, next: any) => {
