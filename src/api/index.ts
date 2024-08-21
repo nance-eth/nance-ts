b25e955f7074d57933110a0b3ec64858b3de536c
jigglyjams <jigglyjams@protonmail.com>
2024-08-21 17:19:32 -0400

    feat(api): revamp build system for relative imports, use esbuild and nodemon, remove unused stuff, start API refactor

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,36 @@
-import cors from 'cors';
-import express from 'express';
-import { TspecDocsMiddleware } from 'tspec';
-import { params } from './tspec';
-import api from './api';
-import ish from './nanceish';
-import tasks from './tasks';
+import cors from "cors";
+import express from "express";
+import { TspecDocsMiddleware } from "tspec";
+import { params } from "./tspec";
+import system from "./routes/system";
+import tasks from "./tasks";
+import spaceMiddleware from "./routes/space/middleware";
+import spaceInfo from "./routes/space/info"
 import { discordInitButtonManager } from "./helpers/discord";
 
 const app = express();
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: false }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: false }));
 app.use(cors({
-  maxAge: 86400,
+  maxAge: 86401,
 }));
 
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
-app.use('/ish', ish);
-
-app.use('/tasks', tasks);
-
-app.use('/', api);
-
-app.get('/', (req, res) => {
-  return res.send(`nance-api commit: ${process.env.RAILWAY_GIT_COMMIT_SHA?.substring(0, 7) ?? 'LOCAL'}`);
+// show commit information
+app.get("/", (_, res) => {
+  const commit = process.env.RAILWAY_GIT_COMMIT_SHA?.substring(0, 7);
+  return res.send(`nance-api commit: ${commit ?? "LOCAL"}`);
 });
 
+app.use("/ish", system);
+app.use("/tasks", tasks);
+// app.use("/docs", )
+app.use("/:space", spaceMiddleware);
+app.use("/:space", spaceInfo)
+
 const init = async () => {
-  app.use('/api/docs', await TspecDocsMiddleware(params));
+  app.use("/api/docs", await TspecDocsMiddleware(params));
   await discordInitButtonManager();
 };
 
--- src/dolt/dbConfig.ts
+++ src/dolt/dbConfig.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { DBConfig } from './types';
 import { DOLT_CERT } from '../keys';
 
-const dbOptionsHosted = (repo: string): DBConfig => {
+export const dbOptions = (repo: string): DBConfig => {
   return {
     database: repo,
     host: process.env.DOLT_HOST,
@@ -14,18 +14,4 @@ const dbOptionsHosted = (repo: string): DBConfig => {
       ca: DOLT_CERT,
     }
   };
-};
-
-const dbOptionsTEST = (repo: string): DBConfig => {
-  return {
-    database: repo,
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: 3306,
-  };
-};
-
-export const dbOptions = (repo: string): DBConfig => {
-  return (process.env.TEST) ? dbOptionsTEST(repo) : dbOptionsHosted(repo);
 };
