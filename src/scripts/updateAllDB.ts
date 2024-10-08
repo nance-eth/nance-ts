44b9b263186f22cfae409e927a90ac0b33166c43
jigglyjams <jigglyjams@protonmail.com>
2024-10-08 15:09:15 -0400

    fix(updateAllDb): fix script for new pools method

--- src/scripts/updateAllDB.ts
+++ src/scripts/updateAllDB.ts
@@ -1,6 +1,6 @@
 import { DoltSysHandler } from '../dolt/doltSysHandler';
 import { DoltHandler } from '../dolt/doltHandler';
-import { pools } from '../dolt/pools';
+import { getDb, getSysDb, initializePools } from '../dolt/pools';
 import { sleep } from "../utils";
 import { oneLine } from "common-tags";
 
@@ -9,11 +9,11 @@ const QUERY = oneLine`
 `;
 
 async function main() {
-  await sleep(2000);
-  const doltSys = new DoltSysHandler(pools.nance_sys);
+  initializePools;
+  const doltSys = getSysDb();
   const spaces = await doltSys.getAllSpaceNames();
   spaces.forEach(async (config) => {
-    const dolt = new DoltHandler(pools[config.space], '');
+    const dolt = getDb(config.space);
     dolt.localDolt.db.query(QUERY).then((res) => {
       console.log(res);
     }).catch((err) => {
